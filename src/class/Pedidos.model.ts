858ec84ebd2b1f57328a34cb9f6b7b1ae6f397cd
javierhol <javierholguin596@gmail.com>
2023-04-18 18:42:23 -0500

    ADD: new function for validate procces

---
+++ src/class/Pedidos.model.ts
@@ -0,0 +1,19 @@
+import ManagePedidos from "../controllers/GestionPedidos";
+
+
+
+
+class PedidosValiadation extends ManagePedidos {
+
+    
+    public ValidateBodega (){}
+
+    private ValidateProvedor (){}
+
+    private ValidateProducto (){}
+
+    private ValidateInventario (){}
+
+}
+
+export default PedidosValiadation;
--- src/controllers/GestionPedidos.ts
+++ src/controllers/GestionPedidos.ts
@@ -3,6 +3,9 @@ import SchemaPedidos from "../models/modelPedidos";
 import { IPedidos } from "../interfaces/pedidos";
 import jwt from "jsonwebtoken";
 import { SECRET } from "../config/config";
+import subProductoSchema from "../models/SubProductos.model";import PedidosValiadation from "../class/Pedidos.model";
+ ;
+
 
 abstract class ManagePedidos {
   public async postPedidos(
@@ -14,44 +17,11 @@ abstract class ManagePedidos {
       const tokenAccesId: any = req.headers["x-id-token"];
       const verifyToken: any = jwt.verify(tokenAccesId, SECRET);
       const idTokenAdmin = verifyToken.id;
+      await new PedidosValiadation().ValidateBodega(
+        //idTokenAdmin,
 
-      let [id_inventario, id_provedor, id_subproducto]   = [req.body.id_inventario, req.body.id_provedor, req.body.id_subproducto];
+      );
 
-      const { company, cantidad, tipo, totalCompra } = req.body;
-      const inventario = await SchemaPedidos.find({ id_inventario });
-      if (inventario.length > 0){
-           const provedor = await SchemaPedidos.find({ id_provedor });
-        if(provedor.length > 0){
-        res.status(404).json({ message: "PROVEDOR_NOT_FOUND" });
-      }
-         
-      }
-        // return res.status(404).json({ message: "INVENTARIO_NOT_FOUND" });
-
-   
-
-      const subproducto = await SchemaPedidos.find({
-        id_inventario: id_subproducto,
-      });
-      if (subproducto.length > 0)
-        return res.status(404).json({ message: "SUBPRODUCTO_NOT_FOUND" });
-
-      if (!idTokenAdmin){
-        return res.status(404).json({ message: "TOKEN_NOT_FOUND" });
-      }else{
-        const newPedidos: IPedidos = new SchemaPedidos({
-          id_inventario,
-          id_provedor,
-          id_subproducto,
-          company,
-          cantidad,
-          tipo,
-          totalCompra,
-          idTokenAdmin,
-        });
-        const pedidos = await newPedidos.save();
-        return res.status(200).json({ message: "PEDIDOS_CREATED", pedidos });
-      }
     } catch (error) {
       return res.status(500).json({ message: "INTERNAL_SERVER_ERROR", error });
     }
@@ -133,3 +103,4 @@ abstract class ManagePedidos {
 }
 
 export default ManagePedidos;
+  
