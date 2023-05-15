5ec9f8f4183cfe31ff1f9712bff6ee3a86cc3704
javierhol <javierholguin596@gmail.com>
2023-05-15 12:58:10 -0500

    ADD: new validation and solution errors

--- src/controllers/GestionInicio.ts
+++ src/controllers/GestionInicio.ts
@@ -2,10 +2,11 @@ import { Request, Response, NextFunction, json } from "express";
 import ProductSchema from "../models/modelProduct";
 import CategorySchema from "../models/CategoryM";
 import ProviderSchema from "../models/modelProviders";
-import { Product } from "../interfaces/product";
 import jwt from "jsonwebtoken";
 import { SECRET } from "../config/config";
 import InventorySchema from "../models/modelInventario";
+import PedidosSchema from "../models/modelPedidos";
+import NotificationSchema from "../models/modelNotfication";
 class AllModules {
   public async getModules(
     req: Request | any,
@@ -13,55 +14,38 @@ class AllModules {
     next: NextFunction
   ): Promise<Request | Response | any> {
     try {
-      const TokenCreate: string = req.params.id!;
+      const TokenCreate: string = req.headers["x-id-token"]!;
       const veryfyToken: Array<any> | any = jwt.verify(TokenCreate, SECRET)!;
       const tokenIdUser = veryfyToken.id;
-      console.log(tokenIdUser);
+      //console.log(tokenIdUser);
 
       if (!tokenIdUser) {
         return res.json({
           message: "El token no existe!",
         });
       } else {
-        const dataProduct = await ProductSchema.find({
-          tokenIdUser,
-        });
-        const dataSumProduct = await ProductSchema.aggregate([])
-          .match({ tokenIdUser })
-          .group({ _id: null, total: { $sum: "$quantity" } });
-        const dataCategory = await CategorySchema.find({ tokenIdUser });
-        const dataProductCategory = await CategorySchema.distinct(
-          "categories",
-          (err: any, categorias: any) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-
-            categorias.forEach(async (categoria: any) => {
-              const productosCategory = await CategorySchema.find({
-                categoria,
-              });
-            });
-          }
-        );
-
-        const dataProvider = await ProviderSchema.find({ tokenIdUser });
-        const dataInventary = await InventorySchema.aggregate([])
-          .match({ tokenIdUser })
-          .group({ _id: null, total: { $sum: "$quantity" } });
+        const dataProduct = await ProductSchema.find({ });
+        const dataCategory = await CategorySchema.find({ });
+        const dataProvider = await ProviderSchema.find({ });
+        const dataInventary = await InventorySchema.find({ });
+        const dataPedidos = await PedidosSchema.find({ });
+        const dataNotify = await NotificationSchema.find({  });
 
         return res.status(200).json({
           ok: true,
           dataCategory,
-          dataProductCategory,
           dataProduct,
           dataProvider,
-          dataSumProduct,
           dataInventary,
+          dataPedidos,
+          dataNotify,
+          
         });
       }
+    
     } catch (error) {
+      console.log(error);
+
       return res.status(500).json({ error, message: "ERROR_SERVER" });
     }
   }
