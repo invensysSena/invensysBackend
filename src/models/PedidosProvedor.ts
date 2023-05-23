a5bdec182ae7ee61c09ae651c93bb40592b2cf97
Miguel Ibarra <miguelgoo205@gmail.com>
2023-05-23 11:46:30 -0500

    j

--- src/models/PedidosProvedor.ts
+++ src/models/PedidosProvedor.ts
@@ -3,10 +3,11 @@ import { IPedidoProvider } from "../interfaces/IpedidosProvedor";
 
 const PedidoProvider = new mongoose.Schema(
   {
-    nr: { type: Number, required: true },
+    NR: { type: Number, required: true },
     fecha: { type: String, required: true },
     cantidadProductos: { type: Number, required: true },
     totalComprap: { type: Number, required: true },
+    
   },
   { timestamps: true }
 );
