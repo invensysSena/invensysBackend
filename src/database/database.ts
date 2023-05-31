4cee86c00c40c1e8ed33a2bf06dd3b42307a259c
Miguel Ibarra <miguelgoo205@gmail.com>
2023-05-31 09:59:03 -0500

    src

--- src/database/database.ts
+++ src/database/database.ts
@@ -12,7 +12,7 @@ export class Conexion {
   public readonly host?: string | any = "localhost";
   private readonly user?: string | any = "root";
   private readonly password?: string | any = "";
-  protected readonly database: string | any = "invensys";
+  protected readonly database: string | any = "storedv1";
   private readonly charset: string | any = "utf8";
   private readonly port: Number | any = 3306;
 
