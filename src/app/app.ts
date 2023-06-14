75eb2461c41d75814e6d56e660e5e4a6c47fe542
Miguel Ibarra <miguelgoo205@gmail.com>
2023-06-14 06:20:35 -0500

    f

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,21 +3,11 @@ import mongoose from "mongoose";
 import path from "path";
 import { PORT } from "../config/config";
 import cors from "cors";
-import RouterUser from "../router/router";
-import RouterProducts from "../router/router.products";
-import RouterCategory from "../router/router.category";
-import RouterProviders from "../router/router.providers";
-import RouterInicio from "../router/router.inicio";
-import RouterNotification from "../router/router.Notification";
-import RouterInventory from "../router/router.inventary";
 import { connect } from "../database/mongodb";
-import RouterPedidos from "../router/router.pedidos";
-import { getLogger } from "nodemailer/lib/shared";
 import { ServerRoutes } from "../utils/ServerRoutes";
 mongoose.set("strictQuery", true);
 const AppServer: express.Application = express();
 import { conexion } from "../database/database";
-import stripe from "stripe";
 class App {
   public startServer = async () => {
     try {
@@ -25,11 +15,7 @@ class App {
       const statusCors: number = 200;
       const port: Number = 8080;
       AppServer.use(
-        cors({
-          origin: [urlConnectionAcceso, "http://localhost:3000"],
-          methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-          optionsSuccessStatus: statusCors,
-        })
+        cors({origin:"*"})
       );
       AppServer.use(express.static(path.join(__dirname, "public")));
       AppServer.use(express.json());
